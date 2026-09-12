2ff29c95f0cefb9961b201c851e743a68849ac11
Daniele <carlucci.daniele@gmail.com>
2026-09-12 17:54:14 +0000

    start

--- start-backend.sh
+++ start-backend.sh
@@ -1,2 +1,14 @@
+#!/bin/bash
+java_pid=$(ps aux | grep ruota | grep RuotaApplication | awk '!/awk/ {print $2}')
+
+if [ -z "$java_pid" ]; then
+    echo "Nessun processo Java trovato con il nome: $process_name"
+else
+    echo "Terminazione del processo Java con PID: $java_pid"
+    kill -9 "$java_pid"
+fi
+
 cd backend
+rm log.log
+rm err.log
 nohup mvn spring-boot:run  > log.log 2>err.log &
--- start-frontend.sh
+++ start-frontend.sh
@@ -1,3 +1,4 @@
+sudo kill -9 $(sudo lsof -t -i:4800)
 cd frontend
 npm install
 nohup npm start &
